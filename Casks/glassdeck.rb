5fcc8cc9de57cccea74c18e242bea6065a90bfce
Francesco Cataldo <francy111200@gmail.com>
2026-08-31 00:03:58 +0200

    chore: point the glassdeck cask at 1.5.0

--- Casks/glassdeck.rb
+++ Casks/glassdeck.rb
@@ -1,6 +1,6 @@
 cask "glassdeck" do
-  version "1.4.1"
-  sha256 "aa2a9b589118892e1955eda93e9ba3513a0b0db80643b2f1658d3279dec072a3"
+  version "1.5.0"
+  sha256 "66434f583a0cf3aa95ab51c5873f5eb49f217110a8080e22c48e8af8ab5b1c9b"
 
   url "https://github.com/FraCata00/glassdeck/releases/download/v#{version}/GlassDeck.zip",
       verified: "github.com/FraCata00/glassdeck/"
